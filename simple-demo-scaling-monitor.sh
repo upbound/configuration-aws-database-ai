d1035f27cbfcdc5cd7de728302a065655aecf6c6
Yury Tsarev <yury@upbound.io>
2025-09-16 17:11:39 +0200

    Add demo scripts
    Signed-off-by: Yury Tsarev <yury@upbound.io>

---
+++ simple-demo-scaling-monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# simple-demo-scaling-monitor.sh
+
+XR_NAME="rds-metrics-database-ai-scale"
+
+while true; do
+    echo "=== $(date '+%Y-%m-%d %H:%M:%S') ==="
+    echo "$ kubectl get xsqlinstance $XR_NAME -o yaml | yq '.metadata.annotations |
+del(.[\"kubectl.kubernetes.io/last-applied-configuration\"])'"
+
+    kubectl get xsqlinstance $XR_NAME -o yaml | yq '.metadata.annotations |
+del(.["kubectl.kubernetes.io/last-applied-configuration"])'
+
+    echo ""
+    sleep 15
+done
