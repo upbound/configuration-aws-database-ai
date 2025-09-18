534847d0a0c904a7eb2b59d223a754d6dc83aac8
Yury Tsarev <yury@upbound.io>
2025-09-18 05:56:41 +0200

    More frequent cron, context safe demo script
    Signed-off-by: Yury Tsarev <yury@upbound.io>

--- simple-demo-scaling-monitor.sh
+++ simple-demo-scaling-monitor.sh
@@ -2,13 +2,14 @@
 # simple-demo-scaling-monitor.sh
 
 XR_NAME="rds-metrics-database-ai-scale"
+CONTEXT="kind-up-configuration-aws-database-ai"
 
 while true; do
     echo "=== $(date '+%Y-%m-%d %H:%M:%S') ==="
     echo "$ kubectl get xsqlinstance $XR_NAME -o yaml | yq '.metadata.annotations |
 del(.[\"kubectl.kubernetes.io/last-applied-configuration\"])'"
 
-    kubectl get xsqlinstance $XR_NAME -o yaml | yq '.metadata.annotations |
+    kubectl --context $CONTEXT get xsqlinstance $XR_NAME -o yaml | yq '.metadata.annotations |
 del(.["kubectl.kubernetes.io/last-applied-configuration"])'
 
     echo ""
